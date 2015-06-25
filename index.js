da17b48c59f49d38200af710c6587ab3ba516d5b
Anthony Short <antshort@gmail.com>
2015-06-24 21:40:48 -0700

    First

---
+++ index.js
@@ -0,0 +1,71 @@
+/**
+ * Module dependencies.
+ */
+
+var slice = require('sliced')
+
+/**
+ * This function lets us create virtual nodes using a simple
+ * syntax. It is compatible with JSX transforms so you can use
+ * JSX to write nodes that will compile to this function.
+ *
+ * let node = element('div', { id: 'foo' }, [
+ *   element('a', { href: 'http://google.com' }, 'Google')
+ * ])
+ *
+ * You can leave out the attributes or the children if either
+ * of them aren't needed and it will figure out what you're
+ * trying to do.
+ */
+
+module.exports = element
+
+/**
+ * Create virtual trees of components.
+ *
+ * This creates the nicer API for the user.
+ * It translates that friendly API into an actual tree of nodes.
+ *
+ * @param {*} type
+ * @param {Object} attributes
+ * @param {Array} children
+ * @return {Object}
+ * @api public
+ */
+
+function element (type, attributes, children) {
+  // Default to div with no args
+  if (!type) {
+    throw new TypeError('element() needs a type.')
+  }
+
+  // Skipped adding attributes and we're passing
+  // in children instead.
+  if (arguments.length === 2 && (typeof attributes === 'string' || Array.isArray(attributes))) {
+    children = attributes
+    attributes = {}
+  }
+
+  // Account for JSX putting the children as multiple arguments.
+  // This is essentially just the ES6 rest param
+  if (arguments.length > 2 && Array.isArray(arguments[2]) === false) {
+    children = slice(arguments, 2)
+  }
+
+  children = children || []
+  attributes = attributes || {}
+
+  // passing in a single child, you can skip
+  // using the array
+  if (!Array.isArray(children)) {
+    children = [children]
+  }
+
+  // if you pass in a function, it's a `Component` constructor.
+  // otherwise it's an element.
+  return {
+    type: type,
+    children: children,
+    attributes: attributes
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,74 @@
+/**
+ * @jsx element
+ */
+
+var assert = require('assert')
+var element = require('../')
+
+it('should throw an error for missing type', function(done){
+  try {
+    element()
+  } catch (e) {
+    done()
+  }
+})
+
+it('should set the tagName', function(){
+  assert(element('span').type === 'span')
+})
+
+it('should set attributes', function(){
+  var node = element('div', { name: 'Foo' })
+  assert(node.attributes.name === 'Foo')
+})
+
+it('should set class from a string', function () {
+  var node = element('div', { class: 'foo bar baz' })
+  assert(node.attributes.class === 'foo bar baz')
+})
+
+it('should render styles from a string', function () {
+  var node = element('div', { style: 'text-align:leftheight:10pxwidth:10px' })
+  assert(node.attributes.style === 'text-align:leftheight:10pxwidth:10px')
+})
+
+it('it should allow using array spread on children', function () {
+  var children = [element('span')]
+  var node = element('div', null, [
+    ...children
+  ])
+  assert(node.children.length === 1)
+  assert(node.children[0].type === 'span')
+})
+
+it('should allow a single DOM node as a child', function () {
+  var node = element('div', null, element('span'))
+  assert(node.children[0].type === 'span')
+})
+
+it('should allow children as rest params', function () {
+  var node = element('div', { foo: 'bar' }, 'one', 'two', 'three', 'four')
+  assert.equal(node.children.length, 4)
+})
+
+it('should allow children as rest params with no attrs', function () {
+  var node = element('div', null, 'one', 'two', 'three', 'four')
+  assert.equal(node.children.length, 4)
+})
+
+it('should allow skipping attributes and using an array of children', function () {
+  var node = element('div', ['foo'])
+  assert(node.children[0] === 'foo')
+})
+
+it('should allow skipping attributes and using a single child', function () {
+  var node = element('div', 'foo')
+  assert(node.children[0] === 'foo')
+})
+
+it('render nodes that work with JSX', function(){
+  assert.deepEqual(
+    <div class="one" id="foo">Hello World</div>,
+    element('div', { class: "one", id: "foo" }, ['Hello World'])
+  )
+})
